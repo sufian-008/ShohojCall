98070c804f3a314cdf9edacf7752b9ff8a0e2f6d
sufian008 <m.a.sufian008@gmail.com>
2025-07-12 20:31:53 +0600

    create server

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import connectTOSocket from "./controller/socketManger.js";
+
+import mongoose from "mongoose";
+
+import cors from "cors";
+
+const app =express();
+const server = createServer(app);
+const io = connectTOSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limi:"40k", extended:true}));
+
+app.get("/home", (req, res)=>{
+    return res.json({"hello": "World"})
+});
+
+const start = async () =>{
+    const connectionDB = await mongoose.connect("mongodb+srv://mdrafin008:YaXZsF9jjkNU0rz4@cluster0.to0zdej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log(`MONGO connected DB Host:${connectionDB.connection.host}`);
+    server.listen(app.get("port"), ()=>{
+         console.log("listening");
+    })
+}
+
+start();
